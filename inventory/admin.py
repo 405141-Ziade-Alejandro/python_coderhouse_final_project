3ea072710508e8cd4117201b4c16b36903b21bea
AlejandroZ <alejandroziade@gmail.com>
2026-04-08 21:25:58 -0300

    done the models of the inventary

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(WorkStation)
+admin.site.register(State)
+admin.site.register(SuppliesType)
+admin.site.register(Supply)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class State(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return f"{self.name}"
+
+class WorkStation(models.Model):
+    code = models.IntegerField(unique=True)
+    origin_area= models.CharField(max_length=50)
+    description = models.TextField()
+    date_received = models.DateField(auto_now_add=True)
+    state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    ws_user = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"WS{self.code}"
+
+class SuppliesType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Supply(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.ForeignKey(SuppliesType, on_delete=models.SET_NULL, null=True, blank=True)
+    stock = models.IntegerField()
+    description = models.TextField()
+    location = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}: {self.description}; tenemos un stock de {self.stock}"
